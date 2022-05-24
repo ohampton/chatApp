6c69230c6f3dd04031a2596269c5a1b4521e3ba5
ohampton <44102431+ohampton@users.noreply.github.com>
2022-05-24 16:36:05 -0400

    createChannelCommit

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useChatContext } from 'stream-chat-react'
+import { UserList } from './'
+import { CloseCreateChannel } from '../assets'
+
+
+const ChannelNameInput = ({ channelName = '', setChannelName}) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setChannelName(event.target.value)
+  }
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder="channel-name" />
+      <p>Add Members</p>
+
+    </div>
+  )
+}
 
 const CreateChannel = () => {
   return (
-    <div>CreateChannel</div>
+    <div>
+      <ChannelNameInput/>
+    </div>
   )
 }
 
