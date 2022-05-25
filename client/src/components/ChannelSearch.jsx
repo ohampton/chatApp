036eff3ee0565db960bd0a5d51ab5cd319dbbaec
ohampton <44102431+ohampton@users.noreply.github.com>
2022-05-25 19:02:57 -0400

    preFinalComponentCommit

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -8,12 +8,29 @@ import {SearchIcon} from '../assets';
 
 const ChannelSearch = () => {
 
+    const { client, setActiveChannel } = useChatContext();
     const [query, setQuery] = useState('');
     const [loading, setLoading] = useState(false);
+    const [teamChannels, setTeamChannels] = useState([])
+    const [directChannels, setDirectChannels] = useState([])
 
     const getChannels = async (text) => {
         try {
-            // todo fetch channels
+            const channelResponse = client.queryChannels({
+                type: 'team',
+                 name: { $autocomplete: text }, 
+                 members: { $in: [client.userID]}
+            });
+            const userResponse = client.queryUsers({
+                id: { $ne: client.userID},
+                name: { $autocomplete: text }, 
+            })
+
+            const [channels, { users }] = await Promise.all([channelResponse, userResponse])
+
+            if(channels.length) setTeamChannels(channels)
+            if(users.length) setDirectChannels(users)
+            
         } catch (error) {
             setQuery('')
         }
